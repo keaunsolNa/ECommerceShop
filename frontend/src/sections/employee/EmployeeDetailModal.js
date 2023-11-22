e6d2f3f98d4f19249ff4c24c64c39fd9ca0f42f8
KeaunsolNa <knsol1992@naver.com>
2023-11-22 16:59:44 +0900

    인사 카드 생성, 인사 카드 상세 조회 하나의 컴포넌트로 통합, 관련 사항 수정

--- frontend/src/sections/employee/EmployeeDetailModal.js
+++ frontend/src/sections/employee/EmployeeDetailModal.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-import { useFormik } from 'formik';
+import { useFormik, FormikProvider } from 'formik';
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { Avatar, Button, Grid, IconButton, InputLabel, Stack, TextField } from '@mui/material';
 import axios from 'axios';
@@ -42,9 +42,8 @@ const EmployeeDetailModal = ({ selectedData, handleReload, handleOpen }) => {
     fileId: Yup.string().max(200),
     address: Yup.string().max(200)
   });
-
-  const formik = useFormik({
-    initialValues: {
+  const getInitialValues = () => {
+    return {
       id: isInsert ? '' : selectedData.id,
       password: isInsert ? '' : selectedData.password,
       confirmPassword: isInsert ? '' : selectedData.confirmPassword,
@@ -58,34 +57,39 @@ const EmployeeDetailModal = ({ selectedData, handleReload, handleOpen }) => {
       callNumber: isInsert ? '' : selectedData.callNumber,
       fileId: isInsert ? '' : selectedData.fileId,
       address: isInsert ? '' : selectedData.address
-    },
+    }
+  }
+  const formik = useFormik({
+    initialValues: getInitialValues(),
     enableReinitialize: true,
-    employeeSchema,
+    validationSchema: employeeSchema,
     onSubmit: (values, { setSubmitting }) => {
+
       const data = {
         id: values.id,
         password: values.password,
         confirmPassword: values.confirmPassword,
-        email: values.commonCodeName,
-        state: values.commonLanguageCode,
-        name: values.commonCodeAbbreviation,
-        gender: values.commonForeignName,
-        role: values.systemKorean,
-        birth: values.systemEnglish.format('YYYY-MM-DD'),
-        phoneNumber: values.commonChinese,
-        callNumber: values.systemJapanese,
-        fileId: values.commonOutName,
-        address: values.commonMainCode
+        email: values.email,
+        state: values.state,
+        name: values.name,
+        gender: values.gender,
+        role: values.role,
+        birth: values.birth.format('YYYY-MM-DD'),
+        phoneNumber: values.phoneNumber,
+        callNumber: values.callNumber,
+        fileId: values.fileId,
+        address: values.address
       };
-      // 제출 이후 로직 작성
-      setLoading(true);
 
+      console.log(data)
+      // 제출 이후 로직 작성
       if (values.id === '') {
         delete values.id;
       }
       const response1 = isInsert ? axios.post('/empBase', data) : axios.patch(`/empBase/update`, data);
       Promise.all([response1])
         .then(() => {
+          console.log('after')
           enqueueSnackbar(`저장이 완료되었습니다.`, {
             anchorOrigin: { vertical: 'top', horizontal: 'center' },
             autoHideDuration: 1000
@@ -143,6 +147,7 @@ const EmployeeDetailModal = ({ selectedData, handleReload, handleOpen }) => {
       });
   }, []);
 
+  const { errors, touched, handleSubmit, isSubmitting, setFieldValue, getFieldProps } = formik;
   if (loading) return <Loader />;
   return (
     <MainCard
@@ -161,207 +166,209 @@ const EmployeeDetailModal = ({ selectedData, handleReload, handleOpen }) => {
         </Stack>
       }
     >
-      <form onSubmit={formik.handleSubmit}>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <Grid container spacing={2.5}>
-            <Grid item xs={12}>
-              <Stack spacing={1.25}>
-                <Stack spacing={2.5} alignItems="center">
-                  <Avatar alt="Avatar 1" size="xl" src={avatarImage(`./default.png`)} />
+      <FormikProvider value={formik}>
+        <form onSubmit={formik.handleSubmit}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <Grid container spacing={2.5}>
+              <Grid item xs={12}>
+                <Stack spacing={1.25}>
+                  <Stack spacing={2.5} alignItems="center">
+                    <Avatar alt="Avatar 1" size="xl" src={avatarImage(`./default.png`)} />
+                  </Stack>
                 </Stack>
-              </Stack>
+              </Grid>
+              <Grid item xs={12}>
+                <Stack spacing={1.25}>
+                  <InputLabel>아이디</InputLabel>
+                  <TextField
+                    fullWidth
+                    id="id"
+                    name="id"
+                    placeholder="ID"
+                    value={formik.values.id}
+                    onChange={formik.handleChange}
+                    error={formik.touched.id && Boolean(formik.errors.id)}
+                    helperText={formik.touched.id && formik.errors.id}
+                  />
+                </Stack>
+              </Grid>
+              { isInsert ?
+                <>
+                  <Grid item xs={12}>
+                    <Stack spacing={1.25}>
+                      <InputLabel htmlFor="password">Password</InputLabel>
+                      <TextField
+                        fullWidth
+                        id="password"
+                        name="password"
+                        placeholder="비밀번호"
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                        error={formik.touched.password && Boolean(formik.errors.password)}
+                        helperText={formik.touched.password && formik.errors.password}
+                      />
+                    </Stack>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Stack spacing={1.25}>
+                      <InputLabel htmlFor="confirmPassword">Password</InputLabel>
+                      <TextField
+                        fullWidth
+                        id="confirmPassword"
+                        name="confirmPassword"
+                        placeholder="비밀번호 확인"
+                        value={formik.values.confirmPassword}
+                        onChange={formik.handleChange}
+                        error={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)}
+                        helperText={formik.touched.confirmPassword && formik.errors.confirmPassword}
+                      />
+                    </Stack>
+                  </Grid>
+                </>
+              : null}
+              <Grid item xs={12}>
+                <Stack spacing={1.25}>
+                  <InputLabel>이메일</InputLabel>
+                  <TextField
+                    fullWidth
+                    id="email"
+                    name="email"
+                    placeholder="이메일"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    error={formik.touched.email && Boolean(formik.errors.email)}
+                    helperText={formik.touched.email && formik.errors.email}
+                  />
+                </Stack>
+              </Grid>
+              <Grid item xs={12}>
+                <Stack spacing={1.25}>
+                  <InputLabel>상태</InputLabel>
+                  <TextField
+                    fullWidth
+                    id="state"
+                    name="state"
+                    placeholder="상태"
+                    value={formik.values.state}
+                    onChange={formik.handleChange}
+                    error={formik.touched.state && Boolean(formik.errors.state)}
+                    helperText={formik.touched.state && formik.errors.state}
+                  />
+                </Stack>
+              </Grid>
+              <Grid item xs={12}>
+                <Stack spacing={1.25}>
+                  <InputLabel>이름</InputLabel>
+                  <TextField
+                    fullWidth
+                    id="name"
+                    name="name"
+                    placeholder="코드약명"
+                    value={formik.values.name}
+                    onChange={formik.handleChange}
+                    error={formik.touched.name && Boolean(formik.errors.name)}
+                    helperText={formik.touched.name && formik.errors.name}
+                  />
+                </Stack>
+              </Grid>
+              <Grid item xs={12}>
+                <Stack spacing={1.25}>
+                  <InputLabel>성별</InputLabel>
+                  <TextField
+                    fullWidth
+                    id="gender"
+                    name="gender"
+                    placeholder="성별"
+                    value={formik.values.gender}
+                    onChange={formik.handleChange}
+                    error={formik.touched.gender && Boolean(formik.errors.gender)}
+                    helperText={formik.touched.gender && formik.errors.gender}
+                  />
+                </Stack>
+              </Grid>
+              <Grid item xs={12}>
+                <Stack spacing={1.25}>
+                  <InputLabel>직책</InputLabel>
+                  <TextField
+                    fullWidth
+                    id="role"
+                    name="role"
+                    placeholder="직책"
+                    value={formik.values.role}
+                    onChange={formik.handleChange}
+                    error={formik.touched.role && Boolean(formik.errors.role)}
+                    helperText={formik.touched.role && formik.errors.role}
+                  />
+                </Stack>
+              </Grid>
+              <Grid item xs={12}>
+                <Stack spacing={1.25}>
+                  <InputLabel>생년월일</InputLabel>
+                  <DatePicker
+                    value={formik.values.birth}
+                    format="YYYY-MM-DD"
+                    onChange={(date) => {
+                      formik.setFieldValue('birth', date);
+                    }}
+                  />
+                </Stack>
+              </Grid>
+              <Grid item xs={12}>
+                <Stack spacing={1.25}>
+                  <InputLabel>핸드폰 번호</InputLabel>
+                  <TextField
+                    fullWidth
+                    id="phoneNumber"
+                    name="phoneNumber"
+                    placeholder="핸드폰 번호"
+                    value={formik.values.phoneNumber}
+                    onChange={formik.handleChange}
+                    error={formik.touched.phoneNumber && Boolean(formik.errors.phoneNumber)}
+                    helperText={formik.touched.phoneNumber && formik.errors.phoneNumber}
+                  />
+                </Stack>
+              </Grid>
+              <Grid item xs={12}>
+                <Stack spacing={1.25}>
+                  <InputLabel>집 전화 번호</InputLabel>
+                  <TextField
+                    fullWidth
+                    id="callNumber"
+                    name="callNumber"
+                    placeholder="집 전화 번호"
+                    value={formik.values.callNumber}
+                    onChange={formik.handleChange}
+                    error={formik.touched.callNumber && Boolean(formik.errors.callNumber)}
+                    helperText={formik.touched.callNumber && formik.errors.callNumber}
+                  />
+                </Stack>
+              </Grid>
+              <Grid item xs={12}>
+                <Stack spacing={1.25}>
+                  <InputLabel>주소</InputLabel>
+                  <TextField
+                    fullWidth
+                    id="address"
+                    name="address"
+                    placeholder="주소"
+                    value={formik.values.address}
+                    onChange={formik.handleChange}
+                    error={formik.touched.address && Boolean(formik.errors.address)}
+                    helperText={formik.touched.address && formik.errors.address}
+                  />
+                </Stack>
+              </Grid>
+              <Grid item pt={2} xs={12}>
+                <AnimateButton>
+                  <Button fullWidth variant="contained" type="submit" disabled={isSubmitting}>
+                    저장
+                  </Button>
+                </AnimateButton>
+              </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <Stack spacing={1.25}>
-                <InputLabel>아이디</InputLabel>
-                <TextField
-                  fullWidth
-                  id="id"
-                  name="id"
-                  placeholder="ID"
-                  value={formik.values.id}
-                  onChange={formik.handleChange}
-                  error={formik.touched.id && Boolean(formik.errors.id)}
-                  helperText={formik.touched.id && formik.errors.id}
-                />
-              </Stack>
-            </Grid>
-            { isInsert ?
-              <>
-                <Grid item xs={12}>
-                  <Stack spacing={1.25}>
-                    <InputLabel htmlFor="password">Password</InputLabel>
-                    <TextField
-                      fullWidth
-                      id="password"
-                      name="password"
-                      placeholder="비밀번호"
-                      value={formik.values.password}
-                      onChange={formik.handleChange}
-                      error={formik.touched.password && Boolean(formik.errors.password)}
-                      helperText={formik.touched.password && formik.errors.password}
-                    />
-                  </Stack>
-                </Grid>
-                <Grid item xs={12}>
-                  <Stack spacing={1.25}>
-                    <InputLabel htmlFor="confirmPassword">Password</InputLabel>
-                    <TextField
-                      fullWidth
-                      id="confirmPassword"
-                      name="confirmPassword"
-                      placeholder="비밀번호 확인"
-                      value={formik.values.confirmPassword}
-                      onChange={formik.handleChange}
-                      error={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)}
-                      helperText={formik.touched.confirmPassword && formik.errors.confirmPassword}
-                    />
-                  </Stack>
-                </Grid>
-              </>
-            : null}
-            <Grid item xs={12}>
-              <Stack spacing={1.25}>
-                <InputLabel>이메일</InputLabel>
-                <TextField
-                  fullWidth
-                  id="email"
-                  name="email"
-                  placeholder="이메일"
-                  value={formik.values.email}
-                  onChange={formik.handleChange}
-                  error={formik.touched.email && Boolean(formik.errors.email)}
-                  helperText={formik.touched.email && formik.errors.email}
-                />
-              </Stack>
-            </Grid>
-            <Grid item xs={12}>
-              <Stack spacing={1.25}>
-                <InputLabel>상태</InputLabel>
-                <TextField
-                  fullWidth
-                  id="state"
-                  name="state"
-                  placeholder="상태"
-                  value={formik.values.state}
-                  onChange={formik.handleChange}
-                  error={formik.touched.state && Boolean(formik.errors.state)}
-                  helperText={formik.touched.state && formik.errors.state}
-                />
-              </Stack>
-            </Grid>
-            <Grid item xs={12}>
-              <Stack spacing={1.25}>
-                <InputLabel>이름</InputLabel>
-                <TextField
-                  fullWidth
-                  id="name"
-                  name="name"
-                  placeholder="코드약명"
-                  value={formik.values.name}
-                  onChange={formik.handleChange}
-                  error={formik.touched.name && Boolean(formik.errors.name)}
-                  helperText={formik.touched.name && formik.errors.name}
-                />
-              </Stack>
-            </Grid>
-            <Grid item xs={12}>
-              <Stack spacing={1.25}>
-                <InputLabel>성별</InputLabel>
-                <TextField
-                  fullWidth
-                  id="gender"
-                  name="gender"
-                  placeholder="성별"
-                  value={formik.values.gender}
-                  onChange={formik.handleChange}
-                  error={formik.touched.gender && Boolean(formik.errors.gender)}
-                  helperText={formik.touched.gender && formik.errors.gender}
-                />
-              </Stack>
-            </Grid>
-            <Grid item xs={12}>
-              <Stack spacing={1.25}>
-                <InputLabel>직책</InputLabel>
-                <TextField
-                  fullWidth
-                  id="role"
-                  name="role"
-                  placeholder="직책"
-                  value={formik.values.role}
-                  onChange={formik.handleChange}
-                  error={formik.touched.role && Boolean(formik.errors.role)}
-                  helperText={formik.touched.role && formik.errors.role}
-                />
-              </Stack>
-            </Grid>
-            <Grid item xs={12}>
-              <Stack spacing={1.25}>
-                <InputLabel>생년월일</InputLabel>
-                <DatePicker
-                  value={formik.values.birth}
-                  format="YYYY-MM-DD"
-                  onChange={(date) => {
-                    formik.setFieldValue('birth', date);
-                  }}
-                />
-              </Stack>
-            </Grid>
-            <Grid item xs={12}>
-              <Stack spacing={1.25}>
-                <InputLabel>핸드폰 번호</InputLabel>
-                <TextField
-                  fullWidth
-                  id="phoneNumber"
-                  name="phoneNumber"
-                  placeholder="핸드폰 번호"
-                  value={formik.values.phoneNumber}
-                  onChange={formik.handleChange}
-                  error={formik.touched.phoneNumber && Boolean(formik.errors.phoneNumber)}
-                  helperText={formik.touched.phoneNumber && formik.errors.phoneNumber}
-                />
-              </Stack>
-            </Grid>
-            <Grid item xs={12}>
-              <Stack spacing={1.25}>
-                <InputLabel>집 전화 번호</InputLabel>
-                <TextField
-                  fullWidth
-                  id="callNumber"
-                  name="callNumber"
-                  placeholder="집 전화 번호"
-                  value={formik.values.callNumber}
-                  onChange={formik.handleChange}
-                  error={formik.touched.callNumber && Boolean(formik.errors.callNumber)}
-                  helperText={formik.touched.callNumber && formik.errors.callNumber}
-                />
-              </Stack>
-            </Grid>
-            <Grid item xs={12}>
-              <Stack spacing={1.25}>
-                <InputLabel>주소</InputLabel>
-                <TextField
-                  fullWidth
-                  id="address"
-                  name="address"
-                  placeholder="주소"
-                  value={formik.values.address}
-                  onChange={formik.handleChange}
-                  error={formik.touched.address && Boolean(formik.errors.address)}
-                  helperText={formik.touched.address && formik.errors.address}
-                />
-              </Stack>
-            </Grid>
-            <Grid item pt={2} xs={12}>
-              <AnimateButton>
-                <Button fullWidth variant="contained" type="submit">
-                  저장
-                </Button>
-              </AnimateButton>
-            </Grid>
-          </Grid>
-        </LocalizationProvider>
-      </form>
+          </LocalizationProvider>
+        </form>
+      </FormikProvider>
     </MainCard>
   );
 };
